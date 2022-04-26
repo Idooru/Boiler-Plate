6eb816afb2b45a8a98d5f1f70972d8bf6dfa512d
Idooru <shere1765@gmail.com>
2022-04-26 17:46:24 +0900

    0.4.2.6

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
 import morgan from "morgan";
 import dotenv from "dotenv";
 dotenv.config();
@@ -13,16 +14,43 @@ app.set("port", process.env.PORT || 5000);
 app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.post("/register", (req, res, next) => {
+app.post("/register", (req, res) => {
   const user = new User(req.body);
+
   user.save((err, userInfo) => {
     if (err) return res.json({ sucess: false, err });
     return res.status(200).json({ sucess: true });
+  });
+});
+
+app.post("/login", (req, res) => {
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSucess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다.",
+      });
+    }
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({
+          loginSucess: false,
+          message: "비밀번호가 틀렸습니다.",
+        });
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+        res
+          .cookie("x_auth", user.token)
+          .status(200)
+          .json({ loginSucess: true, userId: user._id });
+      });
+    });
   });
 });
 
